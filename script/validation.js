52d14ce5b8eca7f9803c710d54e0ef108053d38a
ridaarif98 <ridaarif16@gmail.com>
2021-07-01 10:52:31 +0200

    Add validation for email using javascript

---
+++ script/validation.js
@@ -0,0 +1,63 @@
+const usernameEl = document.querySelector('#username');
+const emailEl = document.querySelector('#email');
+const form = document.querySelector('#contact');
+
+const checkEmail = () => {
+  let valid = false;
+  const email = emailEl.value.trim();
+  if (!isRequired(email)) {
+      showError(emailEl, 'Email cannot be blank.');
+  } else if (!isEmailValid(email)) {
+      showError(emailEl, 'Email is not valid.');
+      
+  emailEl.classList.add('invalidTest');
+  } else {
+      showSuccess(emailEl);
+      valid = true;
+  }
+  return valid;
+};
+
+
+const isEmailValid = (email) => {
+  const re = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  return re.test(email);
+};
+
+const isRequired = value => value === '' ? false : true;
+
+const showError = (input, message) => {
+  // get the form-field element
+  const formField = input.parentElement;
+  // add the error class
+  formField.classList.remove('success');
+  formField.classList.add('error');
+  // show the error message
+  const error = formField.querySelector('small');
+  error.textContent = message;
+  // document.getElementById('email').focus();
+};
+
+const showSuccess = (input) => {
+  // get the form-field element
+  const formField = input.parentElement;
+
+  // remove the error class
+  formField.classList.remove('error');
+  formField.classList.add('success');
+
+  // hide the error message
+  const error = formField.querySelector('small');
+  error.textContent = '';
+}
+
+form.addEventListener('submit', function (e) {
+ 
+ // validate fields
+  let isEmailValid = checkEmail();
+  let isFormValid = isEmailValid && isUsernameValid; 
+  if (!isFormValid) {
+    e.preventDefault();
+  }
+});
+
